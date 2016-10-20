dc995df58094eb6b445b67d40c70f53fb6832bdc
Issa Rice <riceissa@gmail.com>
2016-10-20 05:10:15 -0700

    Comment

--- citewebgen.js
+++ citewebgen.js
@@ -1,4 +1,8 @@
 javascript:var Manifest = "Citewebgen, a cite web citation generator.";
+// Some sites, like EconLog, use class="date" multiple times on a page and in
+// an unpredictable order. It may therefore be preferable to not attempt to use
+// class fields and to instead manually enter in some information. Set this to
+// false in such cases.
 var useClasses = true;
 var publisher_map = {
         "arstechnica.com": "Ars Technica",
