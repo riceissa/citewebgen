c367afc39af139e67a14818cbb73ae79feec388e
Issa Rice <riceissa@gmail.com>
2017-10-05 21:25:05 -0700

    Mark dead-url

--- citewebgen.js
+++ citewebgen.js
@@ -173,12 +173,14 @@ function citationWrapper() {
     metadata["url"] = document.querySelectorAll("input")[0].value;
     delete metadata["date"];
     delete metadata["publisher"];
+    metadata["dead-url"] = "no";
   } else if (document.URL.match(/\/\/web\.archive\.org\/web\//)) {
     metadata["archiveurl"] = metadata["url"];
     metadata["url"] = document.querySelectorAll("input#wmtbURL")[0].value;
     var date_part = document.URL.match(/\/\/web\.archive\.org\/web\/\d{8}/)[0].substr(-8);
     metadata["archivedate"] = getDateFromStr(date_part.substr(0,4) + "-" +
       date_part.substr(4,2) + "-" + date_part.substr(6,2));
+    metadata["dead-url"] = "no";
   }
 
   var print_str = "<ref>{{cite web";
