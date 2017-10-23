24296eaa6b4e468eb74d2bdd2c437326ea961d8b
Issa Rice <riceissa@gmail.com>
2017-10-23 11:01:17 -0700

    handle archiveurl

--- markdown-part.js
+++ markdown-part.js
@@ -3,7 +3,11 @@
 function citationWrapper() {
   var metadata = getMetadata();
 
-  var print_str = metadata["url"] + ' "';
+  if ("archiveurl" in metadata) {
+    var print_str = metadata["archiveurl"] + ' "';
+  } else {
+    var print_str = metadata["url"] + ' "';
+  }
 
   if ("author" in metadata) {
     print_str += metadata["author"] + ". ";
@@ -17,6 +21,14 @@ function citationWrapper() {
   if ("date" in metadata) {
     print_str += metadata["date"] + ". ";
   }
+  if ("archiveurl" in metadata) {
+    print_str += "Archived from the original (" +
+      metadata["url"] + ") ";
+    if ("archivedate" in metadata) {
+      print_str += "on " + getDateFromStr(metadata["archivedate"]) + ". ";
+    }
+  }
+
   print_str += "Retrieved " + metadata["accessdate"] + ".";
   // print_str = print_str.trim();
   print_str += '"';
